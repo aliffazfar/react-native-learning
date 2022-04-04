9507234e0a8e61d9368d3a293aa2232d8ced2593
aliff azfar <aliffazfar34@gmail.com>
2022-04-04 11:40:13 +0800

    updating state

--- src/screens/TextScreen.tsx
+++ src/screens/TextScreen.tsx
@@ -1,14 +1,19 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { StyleSheet, Text, TextInput, View } from 'react-native'
 
 const TextScreen: FC = () => {
+  const [password, setPassword] = useState<string>('')
   return (
     <View>
+      <Text>Enter Password:</Text>
       <TextInput
         style={styles.input}
         autoCapitalize='none'
         autoCorrect={false}
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
+      {password.length < 4 ? <Text>Password must be 4 characters</Text> : null}
     </View>
   )
 }
